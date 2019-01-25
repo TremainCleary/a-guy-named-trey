7d049b08c4af1cc789303d7e4d9ef51dfd959269
TremainCleary <46470269+TremainCleary@users.noreply.github.com>
2019-01-25 08:32:19 -0800

    Add project files.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string charactername = "trey";
+            int characterage;
+            characterage = 26;
+
+            Console.WriteLine("There was a young man named" + charactername);
+            Console.WriteLine(" he was" + characterage + " years old");
+            Console.WriteLine("he really liked the name"+charactername);
+            Console.WriteLine("but he did like being" + characterage);
+
+            Console.ReadLine();
+        }
+    }
+}
